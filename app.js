04ad24bc77fca7652d42af463543449fe174d86f
msalli <alli.cernoch@gmail.com>
2014-07-21 16:39:51 -0700

    Created paths for all pages

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var express = require('express'),
+  ejs = require('ejs'),
   app = express();
 
+
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res) {
-  res.send("Hello World");
+  res.send("Homepage");
 });
+
+app.get('/articles', function(req, res) {
+  res.send("Summary");
+});
+
+app.get('/articles/new', function(req, res) {
+  res.send("Form for adding a new article.")
+});
+
+app.get('/articles/about', function(req, res) {
+  res.send("About Page");
+});
+
+app.get('/articles/contact', function(req, res) {
+  res.send("Contact Page");
+})
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("SERVER STARTED ON local")
